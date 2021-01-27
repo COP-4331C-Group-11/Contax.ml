f6946470df35756f21228907903a72f0e35601b6
Joe Brashear <joebrashear@live.com>
2021-01-27 15:35:02 -0500

    Fixed Login, and shows incorrect login, however not going to the next page?

--- src/js/code.js
+++ src/js/code.js
@@ -1,4 +1,4 @@
-var urlBase = '';
+var urlBase = 'api';
 // do not know our URL yet 
 var extension = 'php';
 // so we have acess to the php files
@@ -12,72 +12,68 @@ var phoneNumber =0;
 //this is going to capture the log in information
 function doLogin()
 {
-var userId = 0;
-var firstName = "";
-var lastName = "";
-var phoneNumber = 0;
+  //now get the log in and password information from the id status
 
-//now get the log in and password information from the id status
+  var login = document.getElementById("loginName").value;
+  var password = document.getElementById("loginPassword").value;
 
-var login = document.getElementById("logUsername").value;
-var password = document.getElementById("logPass").value;
+  // creating the json package for the registration 
+  document.getElementById("loginResult").innerHTML = "";
 
-// creating the json package for the registration 
-
-// this is the json packaging part 
-var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
-var url = urlBase + '/Login.' + extension;
-
-// this is where the package will check matching 
-var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-  try
-  {
-    hr.send(jsonPayload);
-		
-		var jsonObject = JSON.parse( xhr.responseText );
-		
-		userId = jsonObject.id;
-		
-    if( userId < 1 )
-    {
-      // this is the username does not match 
-      // go back into html and create a span that tell if the result is // sucessful or not 
-      document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-			return;
-    }
-    // this is the registration information that is needed
-    firstName = jsonObject.firstName;
-    lastName = jsonObject.lastName;
-    saveCookie();
-	
-		window.location.href = "data.html";
-  }
-  catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-  }
-}
-
-//function for registration json package 
-function doReg()
-{
-  var userId = 0;
-  var firstName = "";
-  var lastName = "";
-
-  // we also have a registration section, create jason package 
-  var registerLog = document.getElementById("regUsername").value;
-  var registerLog = document.getElementById("regPass").value; 
-  
-  // creating the jason package for the registration 
-  // this is the jason packaging part 
+  // this is the json packaging part 
   var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
   var url = urlBase + '/Login.' + extension;
 
   // this is where the package will check matching 
   var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, false);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try
+  {
+    xhr.send(jsonPayload);
+    
+    var jsonObject = JSON.parse( xhr.responseText );
+    
+    userId = jsonObject.id;
+    
+    if( userId < 1 )
+    {
+      // this is the username does not match 
+      // go back into html and create a span that tell if the result is // sucessful or not 
+      document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+      return;
+    }
+    // this is the registration information that is needed
+    firstName = jsonObject.firstName;
+    lastName = jsonObject.lastName;
+    saveCookie();
+  
+    window.location.href = "../data.html";
+  }
+  catch(err)
+  {
+    document.getElementById("loginResult").innerHTML = err.message;
+  }
+}
+
+  //function for registration json package 
+  function doReg()
+  {
+    var userId = 0;
+    var firstName = "";
+    var lastName = "";
+
+    // we also have a registration section, create jason package 
+    var registerLog = document.getElementById("regUsername").value;
+    var registerLog = document.getElementById("regPass").value; 
+    
+    // creating the jason package for the registration 
+    // this is the jason packaging part 
+    var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+    var url = urlBase + '/Login.' + extension;
+
+    // this is where the package will check matching 
+    var xhr = new XMLHttpRequest();
     xhr.open("POST", url, false);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
     try
