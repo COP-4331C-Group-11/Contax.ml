38725b917a75c44dc1d3945c9fe7a58d0f88f643
Jacob <jlfeigenheimer@gmail.com>
2021-02-02 12:12:53 -0500

    Added addcontact.php and additional functions to functions.php

--- src/api/functions.php
+++ src/api/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+/* --- Sign-up Functions --- */
 function emptyInputSignup($username, $password, $password2) {
     $result;
 
@@ -66,3 +67,119 @@ function createUser($conn, $username, $password) {
     header("location : ../signup.php?error=none");
     exit();
 }
+/* --- Sign-up Functions --- */
+
+/* --- Search Functions --- */
+function getContactTable($conn, $userId) {
+    $sql = "SELECT * FROM contacts WHERE userId = ". $userId;
+    return $conn->query($sql);
+}
+  
+function sqlTableToArray($table) {
+    $retval = array();
+  
+    if ($table->num_rows > 0)
+      while($row = $table->fetch_assoc())
+        array_push($retval, $row);
+  
+    return $retval;
+}
+
+function searchByString($table, $searchStr) {
+    $result = array();
+  
+    foreach($table as $row)
+      if (str_contains($row["firstName"], $searchStr) || str_contains($row["lastName"], $searchStr))
+        array_push($result, $row);
+  
+    return $result;
+}
+
+/* --- Search Functions --- */
+
+/* --- Add Contact Functions --- */
+function hasName($fname, $lname){
+    
+    $result;
+
+    if(empty($fname) && empty($lname))
+    {
+       $result = false;
+    }
+    else
+    {
+       $result = true;
+    }
+
+    return $result;
+}
+
+function searchTable($table, $fname, $lname, $phone)
+{
+   
+    foreach($table as $row)
+     {
+        if( (strcmp(strtolower($row["firstName"]), strtolower($fname)) == 0) && (strcmp(strtolower($row["last"]), strtolower($lname)) ==0))
+        {
+            return 1;
+        }  
+        if( !empty($phone) && (strcmp($row["phone"],$phone) ==0))
+        {
+            return 2;
+        }
+    }
+
+    return 0;
+}
+
+function validEmail($email)
+{
+    $pattern1 = "/^([[:alnum:]]+)@([[:alnum:]])+.([[:alnum:]])+$/i";
+
+    if(preg_match($pattern1, $phonenum) == 1)
+        return true;
+    else    
+        return false;
+}
+
+function validPhone($phonenum)
+{
+    $pattern1 = "/^(([0-9]{3})?[0-9]{7}$)/";
+    $pattern2 = "/^(([0-9]{3}-)?[0-9]{3}-[0-9]{4}$)/";
+
+    if(preg_match($pattern1, $phonenum) || preg_match($pattern2, $phonenum))
+    {    return true;}
+    else    
+    {    return false;}
+}
+function sanitizePhone($phonenum)
+{
+    $phone = "";
+    $pattern1 = "/^(([0-9]{3})?[0-9]{7}$)/";
+    $pattern2 = "/^(([0-9]{3}-)?[0-9]{3}-[0-9]{4}$)/";
+    if (preg_match($pattern1, $phonenum))
+        return $phonenum;
+    elseif (preg_match($pattern2, $phonenum))
+    {
+        $phone = preg_replace("/-/","",$phonenum);
+        return $phone;
+    }
+}
+
+function createContact($conn,$userId,$fname,$lname,$phonenum,$email,$date) {
+    $sql = "INSERT INTO contacts (userid, firstName, last, phone, email, dateCreated) VALUES (?, ?, ?, ?, ?, ?) ;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        returnMessage("error", "Error: Could not prepare contact.");
+        exit();
+    }
+
+
+    #leaving phone number as a string incase given an empty string
+    mysqli_stmt_bind_param($stmt, "isssss", $userId, $fname, $lname, $phonenum, $email, $date);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    returnMessage("success","");
+}
+/* --- Add Contact Functions --- */
