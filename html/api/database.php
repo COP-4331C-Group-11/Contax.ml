9b26d3b3c44d6a53280b07ba4188314d726bd4ad
Patrick Stoebenau <pstoebenau@knights.ucf.edu>
2021-02-03 18:46:54 -0500

    Added environment variable pasword for server use

--- html/api/database.php
+++ html/api/database.php
@@ -5,6 +5,10 @@ $dbUsername = "root";
 $dbPassword = "password";
 $dbName = "Contax";
 
+// This is for the actual server
+if (isset($_ENV["DB_PASSWORD"]))
+  $dbPassword = $_ENV["DB_PASSWORD"];
+
 $conn = mysqli_connect($server, $dbUsername, $dbPassword, $dbName);
 
 if (!$conn) {
