a07dd57d091132d00ffd40b8b358dddc844bbc36
Patrick Stoebenau <pstoebenau@knights.ucf.edu>
2021-02-16 18:50:58 -0500

    Fixed edit contact

--- html/js/contacts.js
+++ html/js/contacts.js
@@ -27,9 +27,9 @@ async function getContacts(searchStr) {
 // Just creates table from js object
 function updateContactList(table) {
   const tableEl = document.querySelector("#contact-table");
-  let phoneNumber;
   // output data of each row
   for(const row of table) {
+    let phoneNumber;
     const tableRow = tableEl.insertRow();
     tableRow.classList.add("data-row");
     for (const key in row) {
@@ -49,7 +49,7 @@ function updateContactList(table) {
     deleteBttn.classList.add("delete-button");
     deleteBttn.innerHTML = "DELETE";
     // Add event listeners
-    editBttn.addEventListener("click", (e) => editContact(tableRow, editBttn));
+    editBttn.addEventListener("click", (e) => editContact(tableRow, editBttn, phoneNumber));
     deleteBttn.addEventListener("click", (e) => deleteContact(phoneNumber));
     // Insert into html table
     bttnCell = tableRow.insertCell();
@@ -84,7 +84,7 @@ async function deleteContact(phoneNumber) {
   updateTable(searchInput.value);
 }
 
-function editContact(tableRow, editBttn) {
+function editContact(tableRow, editBttn, phoneNumber) {
   tableRow.style.outline = "2px solid black";
   // Make cells editable
   for (let i = 0; i < tableRow.cells.length-1; i++)
@@ -94,25 +94,30 @@ function editContact(tableRow, editBttn) {
   const confirmBttn = document.createElement("BUTTON");
   confirmBttn.classList.add("confirm-button");
   confirmBttn.innerHTML = "CONFIRM";
-  confirmBttn.addEventListener("click", () => confirmEdit(tableRow, editBttn, confirmBttn));
+  confirmBttn.addEventListener("click", () => confirmEdit(tableRow, editBttn, confirmBttn, phoneNumber));
 
   // Replace edit button with confirm button
   editBttn.replaceWith(confirmBttn);
 }
 
-async function confirmEdit(tableRow, editBttn, confirmBttn) {
+async function confirmEdit(tableRow, editBttn, confirmBttn, phoneNumber) {
   let json = {
     userId: readCookie().userId,
     firstName: "",
     lastName: "",
-    phone: "",
+    oldPhone: "",
+    newPhone: "",
     email: "",
   }
 
   // Add edited data to object
-  for(let i = 0; i < tableRow.cells.length-2; i++)
-    json[Object.keys(json)[i+1]] = tableRow.cells[i].innerHTML;
-  
+  json.firstName = tableRow.cells[0].innerText;
+  json.lastName = tableRow.cells[1].innerText;
+  json.newPhone = tableRow.cells[2].innerText;
+  json.email = tableRow.cells[3].innerText;
+  json.oldPhone = phoneNumber;
+  console.log(json);
+
   const apiUrl="api/editcontact.php";
   let response = await fetch(apiUrl, {
     method: "POST",
