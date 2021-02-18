7850ea5d112e269a3fa7f3ad3b7552e979eafda9
Ro-Hanna Jowallah <rohannajowallah@gmail.com>
2021-02-18 17:15:51 -0500

    Register now has cookies

--- html/js/register.js
+++ html/js/register.js
@@ -1,18 +1,23 @@
 
 var form = document.getElementById('reg')
 // wait untill form is submitted
-form.addEventListener('submit',function(e)
-{
+form.addEventListener('submit',(e) =>doReg(e));
+
   // prevent auto-submission 
-  e.preventDefault()
+  function doReg(e){
 
   var login= document.getElementById('logUsername').value;
   var password= document.getElementById('logPass').value;
   var firstName = document.getElementById('firstNameReg').value;
   var lastName = document.getElementById('lastNameReg').value;
-
+  
+  register(login,password,firstName,lastName);
+  }
   // fetch post request
-  fetch('api/signup.php',{
+  async function register(login,password,firstName,lastName)
+  {
+    let response = await fetch('api/signup.php', 
+    {
     method:'POST',
     body: JSON.stringify
     ({
@@ -22,16 +27,25 @@ form.addEventListener('submit',function(e)
       password :password,
       
     }),
-    // adding xml headers
-    headers:{
-      'Content-type' : 'application/json; charset=UTF-8' ,
-    }
-})
-  //getting the promise
-  .then(res => res.text())          
-  .then(text => console.log(text)) 
+//getting the promise
 
+});
   
-  window.location.href = "contactPage.html";
+  let json = await response.json();
 
-})
+  console.log(json);
+
+  if (json.status != "success")
+  {
+    // if failed it will display a message and return out of this function
+    const $message = document.getElementById('message');
+    $message.style.display = "block";
+    return;
+  }
+
+  saveCookie(json.firstName, json.lastName, json.id);
+  //window.location.href = "contactPage.html";
+  console.log(readCookie());
+
+  }
+
